a10c8ace9d53a0c8dd959f62c458fea24a397d82
AlphaIcarus <guillemgvaldivia@gmail.com>
2022-03-25 00:06:35 +0100

    Few modifications

---
+++ src/testing.py
@@ -0,0 +1,15 @@
+'''
+This file will have all the different classes, one for each test, that will implement the different user interactions with the bot.
+
+We want to define an important ammount of possible interactions from user to bot and bot to user, in order to test them and see how the
+bot behaves.
+'''
+
+# Modules and frameworks
+
+import unittest
+
+# Classes from local file system
+
+class test1(unittest.TestCase):
+    
